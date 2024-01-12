964ce2e883f6ece8bfaddaeb23e98566edbb847e
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2024-01-12 02:12:03 +0000

    Updated branch to merge

--- src/front/js/pages/ProfileSpecialist.js
+++ src/front/js/pages/ProfileSpecialist.js
@@ -40,6 +40,7 @@ const ProfileSpecialist = () => {
    
     const token = sessionStorage.getItem('tokenSpecialist');
     const specialistId = sessionStorage.getItem("specialistId")
+    console.log("Funciona pipto", specialistId)
     const { theid } = params
 
  const handlerReturn = () => {
